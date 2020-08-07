3d062a5597c9bee8ba30eeb7da35114cc4ce9219
FateTdd <59795024+FateTdd@users.noreply.github.com>
2020-08-07 11:02:08 +0100

    'homepage'

---
+++ src/main/java/com/algorithm/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.algorithm.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
+public class AdminController {
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+    /**
+     * Set system homepage
+     * @param m
+     * @return
+     */
+    @RequestMapping("/home")
+    public String home(Model m, HttpServletRequest request) {
+
+        return "admin/home";
+    }
+}
