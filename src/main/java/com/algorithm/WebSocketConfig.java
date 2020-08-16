1b039b178a5944ab22bcf5a075494852f89dcb26
FateTdd <59795024+FateTdd@users.noreply.github.com>
2020-08-16 14:06:45 +0100

    'function2'

---
+++ src/main/java/com/algorithm/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.algorithm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+
+/**
+ * @author
+ * @version
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpoint() {
+        return new ServerEndpointExporter();
+    }
+}
